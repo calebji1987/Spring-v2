3fa7e1145316809c34215e8c7a6a710945485b59
kriptonite <kriptonite@kriptonite-Z68M-D2H>
2021-02-06 15:10:03 -0600

    Configuración e inicio del sistema

---
+++ users-app1/src/test/java/com/dev4j/users/UsersApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev4j.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
